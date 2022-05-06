58aee3ce2f68bbae91e25e60df091c015ce109f6
Chiorean Bogdan <chioreanbogdangabriel@gmail.com>
2022-05-06 22:30:20 +0300

    Am scris async/await

--- myApp.js
+++ myApp.js
@@ -1,18 +1,42 @@
 require('dotenv').config();
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+const Schema1 = mongoose.Schema;
+const personSchema = new Schema1({
+  name: { type: String, required: true },//neaparat sa aiba nume "required:true"
+  age: Number,
+  favoriteFoods: [String]
+});
+const Person = mongoose.model('Person', personSchema);//"person" apare in baza de date 
 
 
-let Person;
 
-const createAndSavePerson = (done) => {
-  done(null /*, data*/);
+const createAndSavePerson = async  ()=> {//echivalente
+  const ChioreanBogdan = new Person({ name: "Chiorean Bogdan", age: 25, favoriteFood: ["carne", "oua", "lapte", "legume"] });
+  return ChioreanBogdan.save();
 };
-
-const createManyPeople = (arrayOfPeople, done) => {
-  done(null /*, data*/);
+//createAndSavePerson();
+let arrayOfPeople = [{ name: "Bogdan", age: 25, favouriteFood: ["ou", "carne"] }, { name: "Ana", age: 28, favouriteFood: ["lapte", "ceai"] }];
+const createManyPeople = async  (arrayOfPeople)=> {
+ return  Person.create(arrayOfPeople)
+ 
 };
+try {
+  await createManyPeople(arrayOfPeople)
+} catch (error) {
+  console.log(err);
+}
 
-const findPeopleByName = (personName, done) => {
-  done(null /*, data*/);
+//createManyPeople(arrayOfPeople)
+ 
+const personName = "Bogdan";
+const findPeopleByName = function (personName, done) {
+  Person.find({ name: personName }, function (err, personFound) {
+    if (err) return console.log(err);
+    done(null, personFound);
+    console.log(personFound);
+  });
 };
 
 const findOneByFood = (food, done) => {
